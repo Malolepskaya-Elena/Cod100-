406582783b6f3e641f8e66177afdd35a8119ef4e
Елена Малолепская <03054177@mail.ru>
2022-04-09 14:00:49 +0300

    Initiaal commit

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,11 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatisticsServiceTest {
+
+    @Test
+    void findMax() {
+    }
